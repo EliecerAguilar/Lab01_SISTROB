c7cbb96b28ebece8d331d51e5c35aaed91418ea3
Faf507 <elieaguilar91@gmail.com>
2019-09-23 13:32:52 -0500

    Add files via upload

---
+++ scketck1.ino
@@ -0,0 +1,55 @@
+
+//Desafio 1 Cambiar tiempo de parpadeo
+/*int pin =13, TiempoOn=1000, TiempoOff=2000;
+void setup() {
+  pinMode(pin,OUTPUT);
+}
+void loop() {
+  digitalWrite(pin,HIGH);
+  delay(TiempoOn);
+  digitalWrite(pin,LOW);
+  delay(TiempoOff);
+}*/
+//Desafio 2 Hacer que el led siga los latidos del corazón
+/*int pin =13, TiempoOn=666, TiempoOff=666;
+//1300(equivale a 1.33 milisegundos) dividido entre 2
+void setup() {
+  pinMode(pin,OUTPUT);
+}
+void loop() {
+  digitalWrite(pin,HIGH);
+  delay(TiempoOn);
+  digitalWrite(pin,LOW);
+  delay(TiempoOff);
+}*/
+//Desafio 3 ¿Podrias hacer que el led parpadeara 1 sola vez?
+/*int pin =13, TiempoOn=2, TiempoOff=2;
+void setup() {
+  pinMode(pin,OUTPUT);
+  digitalWrite(pin,HIGH);
+  delay(TiempoOn);
+  digitalWrite(pin,LOW);
+  delay(TiempoOff);
+}*/
+//Desafio 4 Usar variables para definir tiempo de parpadeo
+/*int pin =13, TiempoOn=1000, TiempoOff=1000;
+void setup() {
+  pinMode(pin,OUTPUT);
+}
+void loop() {
+  digitalWrite(pin,HIGH);
+  delay(TiempoOn);
+  digitalWrite(pin,LOW);
+  delay(TiempoOff);
+}*/
+//Desafio 5 Tiempo de percepción
+/*int pin =13, TiempoOn=2, TiempoOff=2;
+void setup() {
+  pinMode(pin,OUTPUT);
+}
+void loop() {
+  digitalWrite(pin,HIGH);
+  delay(TiempoOn);
+  digitalWrite(pin,LOW);
+  delay(TiempoOff);
+}+/
